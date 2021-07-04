65de96c7e933fc231ac0608f923cb2cae4da483a
daniels0009 <36423740+daniels0009@users.noreply.github.com>
2021-07-04 14:26:01 -0500

    Add files via upload

--- Script Paso_paso.sql
+++ Script Paso_paso.sql
@@ -7,3 +7,45 @@ DROP DATABASE prueba;
 
 CREATE DATABASE prueba CHARACTER SET uft8_COLLATE uft8_spanish_ci;
 
+
+CREATE TABLE prooductos(
+idProducto INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(50) NOT NULL,
+precio DECIMAL(10,2) NOT NULL,
+fecha_alta DATE DEFAULT '0000-00-00'
+);
+
+
+SHOW TABLES;
+
+SHOW CREATE TABLE productos;
+
+
+SHOW TABLES;
+
+RENAME TABLE productos TO producto
+
+ALTER TABLE producto ADD existencias INT NOT NULL;
+
+
+SHOW COLUMNS FROM producto;
+
+
+ALTER TABLE producto DROP COLUMN existencias;
+
+SHOW COLUMNS FROM producto;
+
+
+
+ALTER TABLE producto CHANGE precio precios DECIMAL (18,2);
+SHOW COLUMNS FROM producto;
+
+DROP TABLE producto;
+
+SWOW TABLES;
+
+
+
+
+
+
